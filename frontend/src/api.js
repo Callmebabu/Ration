aec2b63002898f66b8df11aaea8796e73e99f436
Callmebabu <harimaxdp@gmail.com>
2025-05-29 13:08:03 +0530

    Initial commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", // Replace with your Django backend API base URL
+  baseURL: "http://localhost:8000/api", 
+  baseURL: "https://ration-a9md.onrender.com/api",
+// Replace with your Django backend API base URL
 });
 
 export default api;
